5f8b008627bcb749573f4f929bc6c0cb3c44b48c
Ahmed <ahmedmouhtar@ymail.com>
2023-06-05 07:39:41 +0200

    wo_edit/update

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -4,16 +4,28 @@ class WorkoutsController < ApplicationController
   end
 
   def create
-    @workout = Workout.create(user_id: current_user.id)
-    @training = Training.create(workout_id: @workout.id)
+    @workout = Workout.new(workout_params)
+    @workout.user = current_user
+    if @workout.save
+      redirect_to edit_workout_path(@workout)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  # def edit
-  #   @workout = Workout.find(params[:id])
-  # end
+  def edit
+    @workout = Workout.find(params[:id])
+  end
 
-  # def update
-  #   @workout = Workout.find(params[:id])
-  #   @workout.update
-  # end
+  def update
+    @workout = Workout.find(params[:id])
+    @workout.update(workout_params)
+    redirect_to edit_workout_path(@workout)
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:name, :description)
+  end
 end
