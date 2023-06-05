2cc731dc14c5e3347d3ce7bbf0ba2d3d95c81ef9
Ahmed <ahmedmouhtar@ymail.com>
2023-06-05 05:48:42 +0200

    new wo

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,19 @@
 class WorkoutsController < ApplicationController
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.create(user_id: current_user.id)
+    @training = Training.create(workout_id: @workout.id)
+  end
+
+  # def edit
+  #   @workout = Workout.find(params[:id])
+  # end
+
+  # def update
+  #   @workout = Workout.find(params[:id])
+  #   @workout.update
+  # end
 end
