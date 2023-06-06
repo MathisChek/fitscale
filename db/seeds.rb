a42be77a6ef89fa3d4e554286666fe12dee470b8
Leo Mondary <leo_24@live.fr>
2023-06-06 12:40:41 +0200

    api calls and heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+require 'net/http'
+require 'json'
+
+# Set the API endpoint URL
+url = URI('https://api.api-ninjas.com/v1/exercises?muscle=biceps')
+
+# Create a new HTTP request
+request = Net::HTTP::Get.new(url)
+
+# Optionally, set headers or add authentication
+request['API-Key'] = vEqmPDsSQJqKTpaOjrbwVg==88FJhtYQjvhNTufV
+
+# Send the request and get the response
+response = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') do |http|
+  http.request(request)
+end
+
+# Parse the response body as JSON
+data = JSON.parse(response.body)
+
+# Process the response data as needed
+puts data
