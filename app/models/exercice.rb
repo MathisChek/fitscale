d055e9477057ece71b981e9923e8744e78880ff6
Leo Mondary <leo_24@live.fr>
2023-06-08 15:00:09 +0200

    actuality nearly done

--- app/models/exercice.rb
+++ app/models/exercice.rb
@@ -2,4 +2,6 @@ class Exercice < ApplicationRecord
   belongs_to :user
 
   has_many :workout_sets, dependent: :destroy
+
+  MUSCLES = ["abdominals", "abductors", "biceps", "calves", "chest", "forearms", "glutes", "hamstrings", "lats", "lower_back", "middle_back", "neck", "quadriceps", "traps", "tricep"]
 end
