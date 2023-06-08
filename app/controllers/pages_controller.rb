965b9427c47f96764b02be8c57507dfcfaa27b5d
Mathis Chekraoui mathis.chekraoui@hotmail.com <mathis.chekraoui@hotmail.com>
2023-06-08 13:13:08 +0200

    front form new done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,5 @@ class PagesController < ApplicationController
   end
 
   def actuality
-    raise
   end
 end
--- app/models/exercice.rb
+++ app/models/exercice.rb
@@ -2,4 +2,9 @@ class Exercice < ApplicationRecord
   belongs_to :user
 
   has_many :workout_sets, dependent: :destroy
+
+  def self.search(query = nil)
+    return self.all if query.nil?
+    self.where("name ILIKE ?", "%#{query}%")
+  end
 end
