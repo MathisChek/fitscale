6645310881189b7154afbbd57a0453ab7fcca6e2
Mathis Chekraoui mathis.chekraoui@hotmail.com <mathis.chekraoui@hotmail.com>
2023-06-14 16:13:17 +0200

    enlever console

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
       @k << key.to_s
       @v << value
     end
-    console
     @actual_sessions = current_user.week_sessions
 
     @hash_session_exo = {
