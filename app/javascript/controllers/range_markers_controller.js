20eb0b2a58f62dfeab1537c0ef0492a2b10e6206
Ahmed <ahmedmouhtar@ymail.com>
2023-06-14 12:30:01 +0200

    index exo init

--- app/javascript/controllers/range_markers_controller.js
+++ app/javascript/controllers/range_markers_controller.js
@@ -1,31 +1,57 @@
 import { Controller } from "@hotwired/stimulus"
-import { clippingParents } from "@popperjs/core"
+import { clippingParents, createPopper } from "@popperjs/core"
 
 // Connects to data-controller="range-markers"
 export default class extends Controller {
-  static targets = [ "flexibility", "muscularEffort", "breathDifficulty", "flexibilityMarker"]
+  static targets = [ "flexibility", "muscularEffort", "breathDifficulty", "flexibilityMarker",
+                      "muscularEffortMarker", "breathDifficultyMarker" ]
+
   connect() {
-    const flexibilityValues = this.getMinMaxValues(this.flexibilityTarget)
-    const flexibilitySpan = document.createElement("span")
-    this.flexibilityMarkerTarget.appendChild(flexibilitySpan)
-    flexibilitySpan.textContent = flexibilityValues[0]
+    const flexibilityValues = this.getRangeValues(this.flexibilityTarget)
 
-    const muscularEffortValues = this.getMinMaxValues(this.muscularEffortTarget)
-    const muscularEffortSpan = document.createElement("span")
-    this.muscularEffortMarkerTarget.appendChild(muscularEffortSpan)
-    muscularEffortSpan.textContent = muscularEffortValues[0]
+    this.addMinValue(flexibilityValues[0], this.flexibilityMarkerTarget)
+    this.addMidValue(flexibilityValues[1], this.flexibilityMarkerTarget)
+    this.addMaxValue(flexibilityValues[2], this.flexibilityMarkerTarget)
 
+    const muscularEffortValues = this.getRangeValues(this.muscularEffortTarget)
 
+    this.addMinValue(muscularEffortValues[0], this.muscularEffortMarkerTarget)
+    this.addMidValue(muscularEffortValues[1], this.muscularEffortMarkerTarget)
+    this.addMaxValue(muscularEffortValues[2], this.muscularEffortMarkerTarget)
 
+    const breathDifficultyValues = this.getRangeValues(this.breathDifficultyTarget)
 
+    this.addMinValue(breathDifficultyValues[0], this.breathDifficultyMarkerTarget )
+    this.addMidValue(breathDifficultyValues[1], this.breathDifficultyMarkerTarget)
+    this.addMaxValue(breathDifficultyValues[2], this.breathDifficultyMarkerTarget)
 
   }
 
-  getMinMaxValues(target) {
+  getRangeValues(target) {
     const minValue = parseInt(target.min)
     const maxValue = parseInt(target.max)
+    const midValue = minValue + maxValue / 2
 
-    return [minValue, maxValue]
+    return [minValue, midValue, maxValue]
+  }
+
+  addMinValue(minValue, target) {
+    const minValueSpan = document.createElement("span")
+
+    target.appendChild(minValueSpan)
+    minValueSpan.textContent = minValue
+  }
+
+  addMidValue(midValue, target) {
+    const midValueSpan = document.createElement("span")
+    target.appendChild(midValueSpan)
+    midValueSpan.textContent = midValue
+  }
+
+  addMaxValue(maxValue, target) {
+    const maxValueSpan = document.createElement("span")
+    target.appendChild(maxValueSpan)
+    maxValueSpan.textContent = maxValue
   }
 
 
